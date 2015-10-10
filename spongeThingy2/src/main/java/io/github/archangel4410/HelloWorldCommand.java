9bb66babe73c3d6abd98a8de6063d05dfdb235f6
archangel4410 <minerva4410@gmail.com>
2015-10-10 17:49:58 -0400

    Added Source Code

--- spongeThingy2/src/main/java/io/github/archangel4410/HelloWorldCommand.java
+++ spongeThingy2/src/main/java/io/github/archangel4410/HelloWorldCommand.java
@@ -1,7 +1,12 @@
 package io.github.archangel4410;
 
+import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.util.Direction;
 import org.spongepowered.api.util.command.CommandException;
 import org.spongepowered.api.util.command.CommandResult;
 import org.spongepowered.api.util.command.CommandSource;
@@ -9,7 +14,9 @@ import org.spongepowered.api.util.command.args.CommandContext;
 import org.spongepowered.api.util.command.spec.CommandExecutor;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
+import org.spongepowered.api.world.extent.Extent;
 
+import java.util.Optional;
 import java.util.Random;
 
 /**
@@ -24,10 +31,12 @@ public class HelloWorldCommand implements CommandExecutor {
             Player player = (Player) src;
             Random r = new Random();
             int x = r.nextInt(2000) - 1000;
-            int y = r.nextInt(128) - 64;
+            int y = r.nextInt(128);
             int z = r.nextInt(2000) - 1000;
             player.setLocationSafely(new Location<World>(player.getLocation().getExtent(), x,y,z));
             src.sendMessage(Texts.of("Mwa-ha-ha-ha"));
+            Location location = player.getLocation().getRelative(Direction.DOWN);
+            location.setBlockType(BlockTypes.PUMPKIN);
         }
         return null;
     }
